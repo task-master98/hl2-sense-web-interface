1b91355b87006e3ad224326976fb6982cedf59f4
Ishaan Roy <ishaanroy@Ishaans-MacBook-Air.local>
2024-08-19 11:50:07 -0600

    Changed protocol to wss

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import traceback
 
 async def send_command(command):
     try:
-        async with websockets.connect("ws://hololens-sense-9bd80b459134.herokuapp.com") as websocket:
+        async with websockets.connect("wss://hololens-sense-9bd80b459134.herokuapp.com") as websocket:
             await websocket.send(command)
             print(f"Sent command : {command}")
     except Exception as e:
